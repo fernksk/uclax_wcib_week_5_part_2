6d7ad04bdc19dcaa5e8ca573f19f9b85f0640e06
fernksk <fl8llia@gmail.com>
2020-10-30 13:55:36 -0700

    story added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 console.log('Hello from js/scripts.js!');
 const customName = document.getElementById('customname');
-const customAge = document.getElementById('customage');
+const customNamE = document.getElementById('customnamE');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
@@ -9,59 +9,69 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob 40 years old, saw the whole thing, but was not surprised — :insertx: was 300 dollars, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText = 'Sarah and Tina went to California for their winter holiday. One day, they decided to go for a walk at night. The streets were full of holiday magic, but it was 35 fahrenheit outside. They spotted a little cafe nearby, so they rushed in to get something warm. The cafe was empty, it was just them and a cashier lady. They ordered :inserta: and :insertb:. The total was 30 dollars, it was a little pricy for just two drinks. At the handoff, they got 3drinks. Now everything make sense, there was a hot chocolate included in their order. So they asked the lady for a refund. She insisted and told them that :insertc: that came with them ordered it. Her answer shooked them but even more so when that lady started to stare at them with a creepy smile on her face...';
+let insertA = ['an americano','a latte','classic tea'];
+let insertB = ['a cappuccino','herbal tea','a mocha'];
+let insertC = ['the twins','a little girl','a little boy'];
 
 randomize.addEventListener('click', result);
 
 function result() {
   let newStory = storyText;
 
-  let xItem = randomValueFromArray(insertX);
-  let yItem = randomValueFromArray(insertY);
-  let zItem = randomValueFromArray(insertZ);
+  let aItem = randomValueFromArray(insertA);
+  let bItem = randomValueFromArray(insertB);
+  let cItem = randomValueFromArray(insertC);
 
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':inserty:',yItem);
-  newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(':inserta:',aItem);
+  newStory = newStory.replace(':insertb:',bItem);
+  newStory = newStory.replace(':insertc:',cItem);
+
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob',name);
+    newStory = newStory.replace('Sarah',name);
   }
 
-  if(customAge.value !== '') {
-    const age = customAge.value;
-    newStory = newStory.replace('40',age);
+  if(customName.value !== '') {
+    const name = customNamE.value;
+    newStory = newStory.replace('Tina',name);
   }
 
-  if(document.getElementById("us").checked) {
-  $('body').css('background', '#4f478c');
-  }
-
-  if(document.getElementById("eu").checked) {
-    const currency = Math.round(300*0.0714286) + ' euro';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' celsius';
-    newStory = newStory.replace('300 dollars',currency);
-    newStory = newStory.replace('94 fahrenheit',temperature);
-  }
-
-  if(document.getElementById("eu").checked) {
+  if(document.getElementById("jp").checked) {
   $('body').css('background', '#4f878c');
   }
 
   if(document.getElementById("jp").checked) {
-    const currency = Math.round(300*0.0714286) + ' yen';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' celsius';
-    newStory = newStory.replace('300 dollars',currency);
-    newStory = newStory.replace('94 fahrenheit',temperature);
+    const currency = Math.round(30*104.567) + ' yen';
+    const temperature =  Math.round((35-32) * 5 / 9) + ' celsius';
+    newStory = newStory.replace('30 dollars',currency);
+    newStory = newStory.replace('35 fahrenheit',temperature);
+    newStory = newStory.replace('California','Tokyo');
   }
 
-  if(document.getElementById("jp").checked) {
+  if(document.getElementById("eng").checked) {
+  $('body').css('background', '#4f478c');
+  }
+
+  if(document.getElementById("eng").checked) {
+    const currency = Math.round(30*0.771905) + ' pounds';
+    const temperature =  Math.round((35-32) * 5 / 9) + ' celsius';
+    newStory = newStory.replace('30 dollars',currency);
+    newStory = newStory.replace('35 fahrenheit',temperature);
+    newStory = newStory.replace('California','London');
+  }
+
+  if(document.getElementById("nl").checked) {
   $('body').css('background', '#4f678c');
+  }
+
+  if(document.getElementById("nl").checked) {
+    const currency = Math.round(30*0.858390) + ' euros';
+    const temperature =  Math.round((35-32) * 5 / 9) + ' celsius';
+    newStory = newStory.replace('30 dollars',currency);
+    newStory = newStory.replace('35 fahrenheit',temperature);
+    newStory = newStory.replace('California','Amsterdam');
   }
 
   story.textContent = newStory;

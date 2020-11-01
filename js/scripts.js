0cf5558051ea28637af96d70d12227fa7aad4be0
fernksk <fl8llia@gmail.com>
2020-11-01 15:55:02 -0800

    yeee

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'Sarah and Tina went to California for their winter holiday. One day, they decided to go for a walk at night. The streets were full of holiday magic, but it was 35 fahrenheit outside. They spotted a little cafe nearby, so they rushed in to get something warm. The cafe was empty, it was just them and a cashier lady. They ordered :inserta: and :insertb:. The total was 30 dollars, it was a little pricy for just two drinks. At the handoff, they got 3drinks. Now everything make sense, there was a hot chocolate included in their order. So they asked the lady for a refund. She insisted and told them that :insertc: that came with them ordered it. Her answer shooked them but even more so when that lady started to stare at them with a creepy smile on her face...';
+let storyText = 'Sarah and Tina went to California for their winter holiday. One day, they decided to go for a walk at night. The streets were full of life, but it was 35 fahrenheit outside. They spotted a little cafe nearby, so they rushed in to get something warm. The cafe was empty, it was just them and a cashier lady. They ordered :inserta: and :insertb:. The total was 30 dollars, it was a little pricy for just two drinks. At the handoff, they got 3drinks. Now everything make sense, there was a hot chocolate included in their order. So they asked the lady for a refund. She insisted and told them that :insertc: that came with them ordered it. Her answer shooked them but even more so when that lady started to stare at them with a creepy smile on her face...';
 let insertA = ['an americano','a latte','classic tea'];
 let insertB = ['a cappuccino','herbal tea','a mocha'];
 let insertC = ['the twins','a little girl','a little boy'];
@@ -86,6 +86,7 @@ $(document).ready(function() {
 
       $('.reset').hide();
     }
+
     else {
       $('.reset').show();
     }
